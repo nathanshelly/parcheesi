437cb17afd886adef8a0b975a33e9de00109ae6e
Nathan Shelly <nsnshelly@gmail.com>
2017-04-11 15:29:37 -0500

    starting to handle multiple moves and move bonuses

--- src/Parcheesi.ts
+++ src/Parcheesi.ts
@@ -38,27 +38,50 @@ export class Parcheesi implements _Game {
 			let player = this.players[ind];
 			ind = (ind + 1) % this.players.length;
 			
-			// TODO: pass board by value
-			let dice: [number, number] = [Math.floor(6 * Math.random()), Math.floor(6 * Math.random())];
-			let moves: [_Move, _Move] = player.doMove(this.board, dice);
+			// TODO: pass board by value, prevent modifications by player to board
+			let results: [[number, number][], boolean] = this.getTurnDicePairs();
+			let turn_dice_pairs: [number, number][] = results[0];
+			let triple_doubles: boolean = results[1];
 
-			if (this.isCheat(moves, dice, player)){
-				// kick player out of game
-				delete this.board.pawns[player.color];
+			// run each move in order
+			let cheat: boolean = turn_dice_pairs.every(turn_dice_pair => {
+				if(this.handleDiceMove(this.board, turn_dice_pair, player))
+					return false;
+				else
+					return true;
+			});
 
-				let index: number = this.players.indexOf(player);
-				this.players.splice(index, 1);
-			}
-			else {
-				// TODO: reward bops and doubles appropriately, penalize triple doubles
-				let extraMoves = moves.map(m => {
-					this.board.makeMove(m);
-				});
-
-				// idea: write a "do player moves" function that can call itself in the case of extra moves
+			// triple_doubles penalty
+			if(triple_doubles) {
+				// triple doubles -> ya fucked up, move farthest pawn back to start
 			}
 		}
 	};
+
+	handleDiceMove(board: _Board, dice: [number, number], player: _Player): boolean {
+		// ask player for moves
+		let moves: [_Move, _Move] = player.doMove(this.board, dice);
+		// double bonus -> move by tops and bottoms of dice 
+		// double repeat -> 'nother turn
+		// need to give all four dice at once (or handle in player? have to catch that as cheating case)
+
+		if (this.isCheat(moves, dice, player)){
+			// kick player out of game
+			delete this.board.pawns[player.color];
+
+			let index: number = this.players.indexOf(player);
+			this.players.splice(index, 1);
+		}
+		else {
+			// TODO: reward bops and home moves
+			// bop bonus -> if bopped, get extra 20 move
+			// home bonus -> move into home, get extra 10 move 
+			// have board.makeMove() differentiate between case
+			let extraMoves = moves.map(m => {
+				this.board.makeMove(m);
+			});
+		}
+	}
 
 	isCheat(moves: [_Move, _Move], dice: [number, number], player: _Player): boolean {
 		return this.enteredWithoutFive(moves, dice) || this.moveUnenteredPawn(moves) || this.invalidColor(moves, player)
@@ -110,7 +133,7 @@ export class Parcheesi implements _Game {
 		return moves.filter(move => {
 			let hit_blockade = false;
 			let move_board_pawn: BoardPawn = this.board.get_matching_board_pawn(move.pawn);
-			let moved_board_pawn: BoardPawn = new BoardPawn(move_board_pawn.get_position(), move_board_pawn.get_id(), move_board_pawn.get_color());
+			let moved_board_pawn: BoardPawn = new BoardPawn(move_board_pawn.get_position() + move.distance, move_board_pawn.get_id(), move_board_pawn.get_color());
 			blockades.forEach(blockade_pawn => {
 				if(this.board.checkIntersection(blockade_pawn, moved_board_pawn)) {
 					hit_blockade = true;
@@ -124,5 +147,40 @@ export class Parcheesi implements _Game {
 		return !moves.every(m => {
 			return (m.start + m.distance) < 72
 		});
+	}
+
+	getTurnDicePairs(): [[number, number][], boolean] {
+		let potential_dice_pairs: [number, number][] = this.getPotentialTurnDicePairs();
+		let doubles = potential_dice_pairs.map(dice_pair => {
+			return dice_pair[0] == dice_pair[1];
+		});
+
+		let final_turn_dice_pairs: [number, number][] = []
+		final_turn_dice_pairs.push(potential_dice_pairs[0])
+		let triple_doubles: boolean = false;
+
+		if(doubles[0]) {
+			final_turn_dice_pairs.push(potential_dice_pairs[1])
+			if(doubles[1]) {
+				if(doubles[2]) {
+					triple_doubles = true;
+				}
+				else {
+					final_turn_dice_pairs.push(potential_dice_pairs[2])
+				}
+			}	
+		}
+
+		return [final_turn_dice_pairs, triple_doubles]
+	}
+
+	getPotentialTurnDicePairs(): [number, number][] {
+		// generates 3-tuple of dice pairs, the player will play the first one
+		// and potentially the others as governed by getTurnDicePairs
+		return [this.rollTwoDice(), this.rollTwoDice(), this.rollTwoDice()];
+	}
+
+	rollTwoDice(): [number, number] {
+		return [Math.floor(6 * Math.random()), Math.floor(6 * Math.random())]
 	}
 }
