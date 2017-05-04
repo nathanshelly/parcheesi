7126b115a64f1bc58188e00c284eb5ba46f519ad
Nathan Shelly <nsnshelly@gmail.com>
2017-05-04 00:22:28 -0500

    adding isLegal function to interface

--- src/_Move.ts
+++ src/_Move.ts
@@ -1,6 +1,10 @@
 import { Pawn } from './Pawn'
+import { Board } from './Board'
+import { _Player } from './_Player'
 
 // represents a possible move
 export interface _Move {
 	pawn: Pawn;
+
+	isLegal(board: Board, player: _Player, possible_distances: number[], starting_blockades: Pawn[][]): boolean;
 }
