42cc70d34d4f50c55a378e6bf7aab75c3bc0f7d5
Nathan Shelly <nsnshelly@gmail.com>
2017-05-08 18:14:20 -0500

    splitting tests for encoding and decoding

---
+++ test/decoder.test.ts
@@ -0,0 +1,36 @@
+import * as _ from 'lodash'
+import * as e from '../src/Encoder'
+import * as d from '../src/Decoder'
+import * as c from '../src/Constants'
+
+import { Pawn } from '../src/Pawn'
+import { Color } from '../src/Color'
+import { Board } from '../src/Board'
+import { _Player } from '../src/_Player'
+import { Ethernet } from '../src/Ethernet'
+import { PrettyDumbPlayer } from '../src/BasicPlayer'
+import { MoveEnter } from '../src/MoveEnter'
+import { MoveForward } from '../src/MoveForward'
+
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Filename: encoder.test.ts\n\nstartGameXMLToColor tests', () => {
+	it('should return the correct color when given color XML', () => {
+		let red_text = '<start-game>red</start-game>';
+		let blue_text = '<start-game>blue</start-game>';
+		let green_text = '<start-game>green</start-game>';
+		let yellow_text = '<start-game>yellow</start-game>';
+
+		expect(d.startGameXMLToColor(red_text)).to.equal(Color.Red);
+		expect(d.startGameXMLToColor(blue_text)).to.equal(Color.Blue);
+		expect(d.startGameXMLToColor(green_text)).to.equal(Color.Green);
+		expect(d.startGameXMLToColor(yellow_text)).to.equal(Color.Yellow);
+	});
+});
+
+describe('doMoveXMLToBoardDice test', () => { 
+	it('should return a tuple of a board and array of dice', () => {
+		// placeholder
+	});
+});
---
+++ test/encoder.test.ts
@@ -0,0 +1,34 @@
+import * as _ from 'lodash'
+import * as e from '../src/Encoder'
+import * as d from '../src/Decoder'
+import * as c from '../src/Constants'
+
+import { Pawn } from '../src/Pawn'
+import { Color } from '../src/Color'
+import { Board } from '../src/Board'
+import { _Player } from '../src/_Player'
+import { Ethernet } from '../src/Ethernet'
+import { PrettyDumbPlayer } from '../src/BasicPlayer'
+import { MoveEnter } from '../src/MoveEnter'
+import { MoveForward } from '../src/MoveForward'
+
+import { expect } from 'chai';
+import 'mocha';
+
+describe('nameResponseXML tests', () => { 
+	it('should return a name XML when given a name', () => {
+		let names = ['Sasha', 'Nathan', c.TEST_NAME];
+
+		let res = names.every(name => {
+			return '<name>' + name + '</name>' === t.nameResponseXML(name);
+		});
+
+		expect(res).to.equal(true);
+	});
+});
+
+describe('doublesPenalty test', () => { 
+	it('should return <void></void>', () => {
+		expect(d.doublesPenaltyResponse()).to.equal('<void></void>');
+	});
+});
