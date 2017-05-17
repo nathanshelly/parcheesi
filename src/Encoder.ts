738eb9da146c61c0a1353ba83fcf7efc9c81fb4f
Nathan Shelly <nsnshelly@gmail.com>
2017-05-16 22:34:22 -0500

    string literals are nice

--- src/Encoder.ts
+++ src/Encoder.ts
@@ -18,32 +18,32 @@ var parser = new x2js();
 
 /* Wrap a color to send a start-game */
 export function colorToStartGameXML(color: Color): string {
-	return "<start-game>" + Color[color] + "</start-game>";
+	return `<start-game>${Color[color]}</start-game>`;
 }
 
 /* Doubles penalty */
 export function doublesPenaltyXML(): string {
-	return "<doubles-penalty></doubles-penalty>";
+	return `<doubles-penalty></doubles-penalty>`;
 }
 
 export function voidXML(): string {
-	return '<void></void>';
+	return `<void></void>`;
 }
 
 /* Names */
 export function nameResponseXML(name: string): string {
-	return '<name>' + name + '</name>';
+	return `<name>${name}</name>`;
 }
 
 export function movesToMovesXML(moves: _Move[], board: Board): string {
-	return "<moves>" + _.join(moves.map(move => { return moveToMoveXML(move, board); }), "") + "</moves>";
+	return `<moves>${ _.join(moves.map(move => { return moveToMoveXML(move, board); }), "") }</moves>`;
 }
 
 // this whole method is pretty horrifying, try and come back to this
 export function moveToMoveXML(move: _Move, board: Board): string {
 	let move_string, opening_tag, closing_tag;
 	if(move instanceof MoveEnter)
-		move_string = "<enter-piece>" + pawnToXML(move.pawn) + "</enter-piece>";
+		move_string = `<enter-piece>${pawnToXML(move.pawn)}</enter-piece>`;
 	else if(move instanceof MoveForward) {
 		// using test indices here instead of spending time 
 		// building custom functionality to support spec natively
@@ -68,30 +68,30 @@ export function distanceToDistanceXML(distance: number): string {
 }
 
 export function idToIdXML(id: number): string {
-	return '<id>' + id.toString() + '</id>';
+	return `<id>${id}</id>`;
 }
 
 /* Boards r hard */
 export function doMoveToXML(board: Board, distances: number[] /* 1-6 */): string {
-	return "what a lovely board"
+	return `what a lovely board`
 }
 
 /* Dice */
 export function diceToXML(dice: number[]): string {
-	return "<dice>" + _.join(dice.map(dieToXML), "") + "</dice>";
+	return `<dice>${ _.join(dice.map(dieToXML), "") }</dice>`;
 }
 
 export function dieToXML(die: number): string {
-	return "<die>" + die.toString() + "</die>";
+	return `<die>${die}</die>`;
 }
 
 /* Start and home spots */
 export function startSpotsToXML(board: Board): string {
-	return "<start>" + _.range(c.N_COLORS).map(i => pawnsToXML(board.getPawnsOfColorInBase(i))).join("") + "</start>";
+	return `<start>${ _.range(c.N_COLORS).map(i => pawnsToXML(board.getPawnsOfColorInBase(i))).join("") }</start>`;
 }
 
 export function homeSpotsToXML(board: Board): string {
-	return "<home>" + board.getHomeSpots().map(home_spot => { return pawnsToXML(home_spot.getLivePawns()); }).join("") + "</home>";
+	return `<home>${ board.getHomeSpots().map(home_spot => { return pawnsToXML(home_spot.getLivePawns()); }).join("") }</home>`;
 }
 
 /* Main ring and home spots */
@@ -102,12 +102,12 @@ export function mainRingToXML(board: Board): string {
 	let pg = new PawnGetter(true);
 	board.spotRunner(zero, dist, c.COLOR_TO_RUN_MAIN_RING, pg);
 
-	return "<main>" + pawnLocsToXML(pg.pawn_locs) + "</main>";
+	return `<main>${pawnLocsToXML(pg.pawn_locs)}</main>`;
 }
 
 export function homeRowsToXML(board: Board): string {
 	let hrs = board.getHomeRowStarts();
-	return "<home-rows>" + hrs.map(h => {return homeRowToXML(board, h)}).join("") + "</home-rows>";
+	return `<home-rows>${ hrs.map(h => {return homeRowToXML(board, h)}).join("") }</home-rows>`;
 }
 
 export function homeRowToXML(board: Board, hrStart: HomeRowSpot): string {
@@ -122,7 +122,7 @@ export function pawnLocsToXML(pawnLocs: [Pawn, number][]): string {
 }
 
 export function pawnLocToXML(pawnLoc: [Pawn, number]): string {
-	return "<piece-loc>" + pawnToXML(pawnLoc[0]) + "<loc>" + pawnLoc[1].toString() + "</loc></piece-loc>";
+	return `<piece-loc>${ pawnToXML(pawnLoc[0]) }<loc>${ pawnLoc[1] }</loc></piece-loc>`;
 }
 
 /* Pawns */
@@ -131,5 +131,5 @@ export function pawnsToXML(pawns: Pawn[]): string {
 }
 
 export function pawnToXML(pawn: Pawn): string {
-	return "<pawn><color>" + Color[pawn.color] + "</color>" + pawn.id.toString() + "</pawn>";
+	return `<pawn><color>${Color[pawn.color]}</color>${pawn.id}</pawn>`;
 }
